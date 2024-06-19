62cb67f0cd142870397b12f96faf13855155b379
Mikhail Karyagin <notan02091990@gmail.com>
2024-06-19 18:03:55 +0500

    add fastapi

--- next.config.mjs
+++ next.config.mjs
@@ -11,4 +11,9 @@ const withPWA = withPWAInit({
 
 export default withPWA({
   // Your Next.js config
+  source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
 });
